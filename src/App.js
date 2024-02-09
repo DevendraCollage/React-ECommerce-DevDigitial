ee80bd44f26524694e6e78a5ffad7b67ee80f237
Devendra Parmar <devendraparmarcollage@gmail.com>
2024-02-09 18:56:09 +0530

    This is React ECommerce Gadget Purchasing Web App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Home"; //? Home component is in './Home.js'
+import About from "./About"; //? About component is in './About.js'
+import Contact from "./Contact"; //? Contact component is in './Contact.js'
+import Products from "./Products"; //? Products component is in './Products.js'
+import SingleProduct from "./SingleProduct"; //? SingleProduct component is in './SingleProduct.js'
+import Cart from "./Cart"; //? Cart component is in './Cart.js'
+import ErrorPage from "./ErrorPage"; //? ErrorPage component is in './ErrorPage.js'
+import { GlobalStyle } from "./GlobalStyle"; //* This is styled component file
+import { ThemeProvider } from "styled-components";
 
 function App() {
+  // const theme = {
+  //   colors: {
+  //     //* This is represent as an object
+  //     bg: "#000",
+  //   },
+  // };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <GlobalStyle /> //* Now this is work as a external css file
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/singleproduct" element={<SingleProduct />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/errorpage" element={<ErrorPage />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
---
+++ src/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+import styled from "styled-components";
+
+export default function Home() {
+  return <Wrapper className="demo">Home Component</Wrapper>;
+}
+// const Wrapper = styled.section`
+//   background-color: ${({ theme }) => theme.colors.bg};
+//   width: 15rem;
+//   height: 15rem;
+// `;
